53356d94b15f52b7608135f3ae2fbf7f16a209d1
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2023-12-26 22:40:39 +0300

    Grep MVP. Need to add flags process

--- grep/grep.c
+++ grep/grep.c
@@ -1,69 +1,140 @@
 #include "grep.h"
 
-typedef struct arguments {
-  int b, n, s, E, T, v;
-} arguments;
+#include <stdio.h>
 
-arguments parse_arguments(int argc, char **argv, int *idx) {
+// arguments parse_arguments(int argc, char **argv, int *idx) {
+// void add_reg_from_file(arguments *args, char *filepath) {
+//   FILE *f = fopen(filepath, "r");
+//   if (f == NULL) {
+//     if (!args->s) perror(filepath);
+//     exit(EXIT_FAILURE);
+//   }
+//   char *line = NULL;
+//   size_t memlen = 0;
+//   int read = 0;
+//   while ((read = getline(&line, &memlen, f)) != -1) {
+//     // ?
+//   }
+//   free(line);
+//   fclose(f);
+// }
+
+arguments parse_arguments(int argc, char **argv) {
   int opt;
   arguments opts = {0};
 
-  const char *short_options = "bnseEtTv";
+  const char *short_options = "eivclnhsfo";
 
-  const struct option long_options[] = {
-      {"input", no_argument, NULL, 'i'},
-      {"number-nonblank", no_argument, NULL, 'b'},
-      {"number", no_argument, NULL, 'n'},
-      {"squeeze-blank", no_argument, NULL, 's'},
-      {NULL, 0, NULL, 0}};
+  const struct option long_options[] = {{"f", required_argument, NULL, 'f'},
+                                        {NULL, 0, NULL, 0}};
 
   while ((opt = getopt_long(argc, argv, short_options, long_options, NULL)) !=
          -1) {
     switch (opt) {
-      case 'b':
-        opts.b = 1;
+      case 'e':
+        opts.e = 1;
+        opts.pattern = optarg;
+        break;
+      case 'i':
+        opts.i = 1;
+        break;
+      case 'v':
+        opts.v = 1;
+        break;
+      case 'c':
+        opts.c = 1;
+        break;
+      case 'l':
+        opts.l = 1;
         break;
       case 'n':
         opts.n = 1;
         break;
+      case 'h':
+        opts.h = 1;
+        break;
       case 's':
         opts.s = 1;
         break;
-      case 'e':
-        opts.E = 1;
-        opts.v = 1;
+      case 'f':
+        opts.f = 1;
+        // ?
         break;
-      case 'E':
-        opts.E = 1;
-        break;
-      case 't':
-        opts.T = 1;
-        opts.v = 1;
-        break;
-      case 'T':
-        opts.T = 1;
+      case 'o':
+        opts.o = 1;
         break;
       case '?':
-        fprintf(stderr, "Usage: %s [OPTION] [FILE]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [OPTION] template [FILE]\n", argv[0]);
         exit(EXIT_FAILURE);
       default:
         fprintf(stderr, "Unexpected error\n");
         exit(EXIT_FAILURE);
     }
   }
-  (*idx) = optind;
+  if (opts.pattern == NULL) {
+    opts.pattern = argv[optind++];
+  }
+  // (*idx) = optind;
   return opts;
 }
 
-int main(int argc, char *argv[]) {
-  char *line = "Hello world";
+void output_line(char *line, int len) {
+  for (int i = 0; i < len; ++i) {
+    putchar(line[i]);
+  }
+  // ?
+}
 
+void process_file(arguments arg, char *path, regex_t *reg) {
+  FILE *f = fopen(path, "r");
+  if (f == NULL) {
+    if (!arg.s) perror(path);
+    exit(EXIT_FAILURE);
+  }
+  char *line = NULL;
+  size_t memlen = 0;
+  int read = 0;
+
+  while ((read = getline(&line, &memlen, f)) != -1) {
+    int result = regexec(reg, line, 0, NULL, 0);
+    if (result == 0) output_line(line, read);
+  }
+  free(line);
+  fclose(f);
+}
+
+// void print_match(arguments args, regex_t *reg, char *line, char *path,
+//                  int line_count) {
+void print_match(regex_t *reg, char *line) {
+  regmatch_t match;
+  int offset = 0;
+  while (1) {
+    int result = regexec(reg, line + offset, 1, &match, 0);
+    if (result != 0) {
+      break;
+    }
+    // print_flags(arg, path, line_count);
+    for (int i = match.rm_so; i < match.rm_eo; ++i) {
+      putchar(line[offset + i]);
+    }
+    putchar('\n');
+    offset += match.rm_eo;
+  }
+}
+
+void output(int argc, char **argv) {
+  arguments arg = parse_arguments(argc, argv);
   regex_t re;
-  regcomp(&re, "pattern", REG_ICASE);
-  int result = regexec(&re, line, 0, NULL, 0);
+  int error = regcomp(&re, arg.pattern, 0);
+  if (error) perror("Error");
+  for (int i = optind; i < argc; ++i) {
+    process_file(arg, argv[i], &re);
+  }
+}
 
-  printf("%d\n", result);
-  printf("%s\n", line);
+int main(int argc, char *argv[]) {
+  // arguments args = parse_arguments(argc, argv);
+  output(argc, argv);
 
   return 0;
 }
--- grep/grep.h
+++ grep/grep.h
@@ -1,5 +1,19 @@
+#ifndef GREP_H
+#define GREP_H
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "regex.h"
+
+typedef struct arguments {
+  int e, i, v, c, l, n, h, s, f, o;
+  char* pattern;
+
+  // int len_pattern;
+  // int mem_pattern;
+
+} arguments;
+
+#endif
